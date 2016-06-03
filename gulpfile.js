8d09f77ccc94ac620e2c5c269dae4fe3ac3af49d
Ryan Allred <Ryan@Allred.xyz>
2016-06-03 16:14:26 -0600

    Added build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var rename = require('gulp-rename');
 
-// Note, not working on Node 6.
 gulp.task('build', function() {
   return gulp.src('src/power-grid.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
     .pipe(uglify())
+    .pipe(rename('power-grid.min.js'))
     .pipe(gulp.dest('build'));
 });
